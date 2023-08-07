01ccd79ba5613f0bd1190656bd99bcad0891e096
VCadenasu <vlcadenasu@hotmail.com>
2023-08-06 21:42:04 -0700

    adding file 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - Program that gives positives numbers
  * return: 0 (Success)
@@ -16,6 +17,6 @@ int main(void)
 	else if (n == 0)
 	  printf("%d is zero\n", n);
 	else
-	  printf("%d is negative\n", n)
+	  printf("%d is negative\n", n);
 	return (0);
 }
