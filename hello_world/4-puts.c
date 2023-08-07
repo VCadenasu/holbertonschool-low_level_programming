106617255da9f65dd33001dfb29cc63c07aff44e
VCadenasu <vlcadenasu@hotmail.com>
2023-08-06 20:48:14 -0700

    fixing file 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Program that prints lines.
- * return: 0
+ * Return: 0 (succes)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
