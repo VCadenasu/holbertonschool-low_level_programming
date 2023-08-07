2939f15d18876afd22068bf9ba80e6a3e906b96d
VCadenasu <vlcadenasu@hotmail.com>
2023-08-06 21:16:08 -0700

    fixing file 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
  * main - Program that print sizes of computer styles
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float e;
-  printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-  printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-  printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-  printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-  printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
-  return (0);
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
  }
 
