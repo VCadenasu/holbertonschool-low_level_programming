139f9489521944079e6345dbfe66892db6256212
VCadenasu <vlcadenasu@hotmail.com>
2023-08-09 16:18:22 -0700

    first try

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program that puts a script.
+ * return: Always (0)
+ */
+
+int main(void) {
+  printf(\_putchar);
+  return(0)
+    }
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0 - how to program _putchar
