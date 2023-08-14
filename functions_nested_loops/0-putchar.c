64024d7e9a3862dadad608a1d7c3481eaa975dc4
VCadenasu <vlcadenasu@hotmail.com>
2023-08-13 18:27:58 -0700

    add file 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - A program that puts a script.
- * return: Always (0)
+ * return: Always success (0)
  */
 
 int main(void) {
-  printf(\_putchar);
-  return(0)
-    }
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
