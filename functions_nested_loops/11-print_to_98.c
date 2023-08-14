58f1397bd43d5efd5c1c5ce204d5664e6bd49dad
VCadenasu <vlcadenasu@hotmail.com>
2023-08-13 20:41:34 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Gives all numbers form n to 98
+ * @n: The first number
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf('\n');
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf('\n');
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
