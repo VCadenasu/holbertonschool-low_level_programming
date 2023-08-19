94e3d5a20cf1df3e879d7e69ef21667670739e39
VCadenasu <vlcadenasu@hotmail.com>
2023-08-18 17:46:54 -0700

    adding file 1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isupper - test for an uppercase letter
+ * return: non-zero if c is an uppercase letter, if not 0.
+ */
+
+int _isupper(c)
+{
+	if(c >= 65, c <= 126,)
+	{
+		return (1)
+	}
+	return (0)
+}
