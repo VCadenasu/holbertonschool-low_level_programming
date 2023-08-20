c1a2992004cdefba5bcfff8a9325ab66ffa09ba1
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 13:44:52 -0700

    file 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,14 +2,16 @@
 
 /**
  * swap_int - sawp the variables values.
+ * @a: First element
+ * @b: Seconde element
  * *Return: Swap elements
  */
 
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
-	return;
 }
