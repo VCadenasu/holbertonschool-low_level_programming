0b2ee091f4e96d9e64c20b58ff9add3728bb8ffd
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 13:50:01 -0700

    adding file 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Total amount of characte of string
+ * @s: String variable
+ * Return: Lenght of the string.
+ */
+
+int _strlen(char *s)
+{
+	int contador = 0;
+
+	while (s[++contador] != 0);
+	return (contador);
+}
