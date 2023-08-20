a68f68a8c81f4c43658e858d01d5e52b36695ab7
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 16:39:33 -0700

    adding file 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every 2 characters
+ * @str: string
+ * Return: prints every other character of a string
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	putchar(str[0]);
+	len--;
+	for (i = 1; i <= len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar(10);
+}
+
