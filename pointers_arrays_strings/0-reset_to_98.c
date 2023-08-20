27f7ace21cffd5fcb7a646dd53f4244d8a58abfe
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 13:31:18 -0700

    file 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - Puts a differente element to int.
+ * @n: Input number.
+ * Return: Elemente int.
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
