418f8ac49fd8c5baf13b66eb46e1b1e2c543471b
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 19:46:11 -0700

    main

--- recursion/main.h
+++ recursion/main.h
@@ -1,3 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
@@ -11,3 +16,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
 int is_prime_number(int n);
+
+#endif
