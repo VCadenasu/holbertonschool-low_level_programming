c32bc7bf8b41ad4390c62b64315f6e1ea5ff4486
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 19:52:45 -0700

    file 1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,13 @@
 
 /**
  * isupper - test for an uppercase letter
+ * @c: text
  * return: non-zero if c is an uppercase letter, if not 0.
  */
 
-int _isupper(c)
+int _isupper(int c)
 {
-	if(c >= 65, c <= 126,)
-	{
-		return (1)
-	}
-	return (0)
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
