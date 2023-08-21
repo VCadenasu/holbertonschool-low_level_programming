5bc1456ca063b0ba58418f032fb82f589d74ce9c
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 19:05:05 -0700

    file 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Gives the total characters of a sting
+ * @s: string
+ * Return: Length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len + _strlen_recursion(s + 1);
+	}
+}
