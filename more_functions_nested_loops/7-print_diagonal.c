cdc5bf0771f3f8a8bacec009b467f1b52263dc1c
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 21:14:21 -0700

    file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: times
+ * result: Print the line
+ */
+
+void print_diagonal(int n)
+{
+	if (m <= 0)
+	{
+		_putchar('\0');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
