7521e190aad8e19c27f521a2cbb961799d256011
VCadenasu <vlcadenasu@hotmail.com>
2023-08-20 20:01:35 -0700

    file 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit
+ * @c: variable
+ * Return: 1 if digit or 0 if not.
+ */
+
+int _isdigit(int c)
+{
+	for (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
