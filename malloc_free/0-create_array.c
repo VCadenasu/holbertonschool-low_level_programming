38ab9c72ec1ecec763289fa11a8c7767e97a39eb
VCadenasu <vlcadenasu@hotmail.com>
2023-08-23 21:01:01 -0700

    file 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - a program to create an array
+ * @size: size of the variable.
+ * @c: character to print.
+ * Return: 0 if null or a pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char* ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+}
