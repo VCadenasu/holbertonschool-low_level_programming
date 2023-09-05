60e77882ce3a06865b519b91d8f80998f2afaf38
VCadenasu <vlcadenasu@hotmail.com>
2023-09-04 20:30:49 -0700

    add file 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct
+ * @d: ponint a struct
+ */
+
+void print_dog(struct dog *d)
+{
+	const char *NIL = "nil";
+
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		printf("Name: (%s)\n", NIL);
+	else
+		printf("Age: %f\n", (*d).name);
+
+	printf("Age: %f\n", (*d).age);
+	if ((*d).owner == NULL)
+		printf("Owner: %s\n", NIL);
+	else
+		printf("owner: %s\n", (*d)owner);
+}
