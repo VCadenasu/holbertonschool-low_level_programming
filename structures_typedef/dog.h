fa5cd5d5aa2dbab6f30b0124f02bef978871da15
VCadenasu <vlcadenasu@hotmail.com>
2023-09-04 19:54:50 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that contains the dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
