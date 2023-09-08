fa173dcbb5a7dd215ab6e2b56106bc351225500b
VCadenasu <vlcadenasu@hotmail.com>
2023-09-08 08:20:30 -0700

    header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <sdlib.h>
+#include <string.h>
 
 /**
  * struct dog - struct that contains the dog info
@@ -8,15 +10,26 @@
  * @owner: dog owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
